054cea5d226dca7797cc1935d4c1295abb22ae25
henrik.carlsson@me.com <henrik.carlsson@me.com@f4e37ef5-9d55-438e-ab7f-4be2c5091528>
2011-02-05 22:43:09 +0000

    Med Pizzasugens egen 4Square Access Token
    git-svn-id: https://pl3.projectlocker.com/mikaeljorhult/Pizzasugen/svn@13 f4e37ef5-9d55-438e-ab7f-4be2c5091528

--- trunk/apps/loader.php
+++ trunk/apps/loader.php
@@ -1,11 +1,12 @@
 <?php
 	define('GOWALLA_API_KEY', 'd4c50ca60731423eb4d72d088dbe780b');
 	define('GOWALLA_SECRET_KEY', 'e95feac52e744046a61fd5dacc889b3f');
-	define('FOURSQUARE_ACCESS_TOKEN', 'RI00RJ5EISDJC43PHZYLLESLKF023ZQOXPF4LAPQVLPE5M3M');
+	define('FOURSQUARE_ACCESS_TOKEN', 'JB0U4S3YPMPCYB0WOU04R2FP5KWGCJJAVATS3WXCZMMPMVFX');
 	define('DEFAULT_RADIUS', 5000);
 	
 	require_once('libraries/spot.php');
 	require_once('libraries/iSpotProvider.php');
 	require_once('libraries/gowalla.php');
 	require_once('libraries/foursquare.php');
+	
 ?>
