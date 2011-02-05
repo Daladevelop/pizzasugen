a3b85c8a79853bb35ec317ced81619f46ca68e24
mikael@jorhult.se <mikael@jorhult.se@f4e37ef5-9d55-438e-ab7f-4be2c5091528>
2011-02-05 21:52:54 +0000

    Add Spot class.
    git-svn-id: https://pl3.projectlocker.com/mikaeljorhult/Pizzasugen/svn@9 f4e37ef5-9d55-438e-ab7f-4be2c5091528

--- trunk/apps/loader.php
+++ trunk/apps/loader.php
@@ -3,7 +3,8 @@
 	define('GOWALLA_SECRET_KEY', 'c3d1b6cd7edc41c5a7692f7732e2f51e');
 	define('FOURSQUARE_ACCESS_TOKEN', 'RI00RJ5EISDJC43PHZYLLESLKF023ZQOXPF4LAPQVLPE5M3M');
 	define('DEFAULT_RADIUS', 5000);
-  
+	
+	require_once('libraries/spot.php');
 	require_once('libraries/iSpotProvider.php');
 	require_once('libraries/gowalla.php');
 	require_once('libraries/foursquare.php');
