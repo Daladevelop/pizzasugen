a838702be0014410e19fa71e3c7bd289b5dbc7b4
mikael@jorhult.se <mikael@jorhult.se@f4e37ef5-9d55-438e-ab7f-4be2c5091528>
2011-02-06 00:14:12 +0000

    Validate values and return json.
    git-svn-id: https://pl3.projectlocker.com/mikaeljorhult/Pizzasugen/svn@24 f4e37ef5-9d55-438e-ab7f-4be2c5091528

--- trunk/find.php
+++ trunk/find.php
@@ -1,12 +1,14 @@
 <?php
 	require_once('apps/loader.php');
 	
-	if(isset($_GET['lat'], $_GET['lng'])) {
+	if(isset($_GET['lat'], $_GET['lng']) && is_numeric($_GET['lat']) && is_numeric($_GET['lng'])) {
 		$latitude= $_GET['lat'];
 		$longitude = $_GET['lng'];
-		$spots = find_spots($lat, $long);
+		$spots = find_spots($latitude, $longitude);
 		
 		$response['spots'] = count($spots);
-		$response['key'] = '';
+		$response['key'] = str_replace('.', '', round($latitude, 2) . round($longitude, 2));
+		
+		echo json_encode($response);
 	}
 ?>
