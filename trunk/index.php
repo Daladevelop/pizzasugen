64291e22cfa975a16057c4881209391608bf9836
rickard@0x539.se <rickard@0x539.se@f4e37ef5-9d55-438e-ab7f-4be2c5091528>
2011-02-08 22:18:47 +0000

    google analytics
    git-svn-id: https://pl3.projectlocker.com/mikaeljorhult/Pizzasugen/svn@44 f4e37ef5-9d55-438e-ab7f-4be2c5091528

--- trunk/index.php
+++ trunk/index.php
@@ -34,7 +34,20 @@
     </p>    
   </div>
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
-  <script type="text/javascript" src="js/geo.js"></script> 
+  <script type="text/javascript" src="js/geo.js"></script>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21274752-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
 </body>
 
 </html>
