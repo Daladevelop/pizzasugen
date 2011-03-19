e488bb302cda352b7411e596b6c6315c8165f1e0
rickard@0x539.se <rickard@0x539.se@f4e37ef5-9d55-438e-ab7f-4be2c5091528>
2011-03-19 22:44:19 +0000

    lite info på första sidan
    git-svn-id: https://pl3.projectlocker.com/mikaeljorhult/Pizzasugen/svn@48 f4e37ef5-9d55-438e-ab7f-4be2c5091528

--- trunk/pages/main.php
+++ trunk/pages/main.php
@@ -4,6 +4,8 @@
   geolocation-data så kommer du få ett svar som talar om vilka
   pizzerior som finns i närheten.</p>
   
+  <p>Tydligen har Twitter avaktiverat geolocation tillfälligt, men att hitta pizza via webbläsaren ska fortfarande gå bra. <a href="http://status.twitter.com/post/3682240237/add-your-location-temporarily-disabled-on-web">http://status.twitter.com/post/3682240237/add-your-location-temporarily-disabled-on-web</a> för mer information</p>
+  
   <p id="geo">Är du inte med i twitter-matchen? Prova att hitta pizzerior genom positionering i webbläsaren. Detta fungerar på de flesta nyare webbläsare, förutom Internet Explorer.<a href="javascript:void(0);" id="findme">Hitta mig</a></p>
   
   <div id="error" class="hidden"></div>
