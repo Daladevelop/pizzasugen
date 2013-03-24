d8ff1427dde41d5b7d6a50a5e447b33f48bc6278
rickard <rickard@montania.se>
2013-03-24 18:38:38 +0100

    Config fil

---
+++ apps/config.dist.php
@@ -0,0 +1,19 @@
+<?php // Rename to config.php and edit values
+
+define('GOWALLA_API_KEY', '');
+define('GOWALLA_SECRET_KEY', '');
+
+define('FOURSQUARE_ACCESS_TOKEN', '');
+
+define('PIZZANU_API_KEY', '');
+define('PIZZANU_SECRET_KEY', '');
+define('PIZZANU_USERNAME', '');
+define('PIZZANU_PASSWORD', '');
+
+define('DEFAULT_RADIUS', 5000);
+define('KEY_LENGTH', 8);
+define('CACHE_LIFETIME', 60*60*4); // Save items in DB max 4h
+
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_DSN', 'mysql:dbname=pizzasugen;host=localhost');
